74fe9575cb9c88dff73ebc73df4f9ddfe78d6564
analistakatia <katiadocita@gmail.com>
2022-06-29 16:26:12 -0300

    Entrega Minima

--- src/desafio01/SistemaSorteio.java
+++ src/desafio01/SistemaSorteio.java
@@ -18,9 +18,11 @@ public class SistemaSorteio {
 
         if (num == valor){
             System.out.println("Você acertou o número exato! Parabéns!!!\nVocê ganhou 10 pontos!");
-        } else if (num == valormenor || num == valormaior) {
-            System.out.println("Você errou por um número!! \nVocê ganhou 5 pontos por quase acertar!");
-        }else {
+        } else if (num == valormaior) {
+            System.out.println("Você errou por um número!! \nPorém, como errou por um número acima, você ganhou 5 pontos por quase acertar!");
+        } else if (num == valormenor) {
+            System.out.println("Você errou por um número!! \nPorém, como errou por um número abaixo, você perdeu 5 pontos :(");
+        } else {
             System.out.println("Poxa...Você não teve muita sorte :( \nMas não desista, na próxima você consegue!!");
         }
         /*Scanner continuar = new Scanner(System.in);
@@ -29,15 +31,31 @@ public class SistemaSorteio {
         System.out.println("2. Sair");
         String escolha = continuar.next();
 
-        while (escolha == "1"){
+        int somaPto1 = acerto + acerto;
+        int somaPto2 = aproximado + acerto;
+        int SomaPto3 = aproximado + aproximado;
 
+        while (escolha == "1"){
+            //Scanner numInformado = new Scanner(System.in);
+            System.out.print("Escolha um número de 1 a 10: ");
+            //int num = numInformado.nextInt();
+            System.out.println("O número sorteado é: " + valor);
+
+            if (num == valor){
+                int tot1 = somaPto1;
+                System.out.println("Você acertou o número exato! Parabéns!!!\nVocê ganhou 10 pontos!");
+                System.out.println("Você está com " + tot1 + " pontos.");
+            } else if (num == valormenor || num == valormaior) {
+                int tot2 =
+                System.out.println("Você errou por um número!! \nVocê ganhou 5 pontos por quase acertar!");
+            }else {
+                System.out.println("Poxa...Você não teve muita sorte :( \nMas não desista, na próxima você consegue!!");
+            }
         }
 
         switch (escolha){
             case "1":
-                int somaPto1 = acerto + acerto;
-                int somaPto2 = aproximado + acerto;
-                int SomaPto3 = aproximado + aproximado;
+
                 while (escolha = )
         }
 
