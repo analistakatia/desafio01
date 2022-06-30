47b5c1e90422ba21576eb0a07176f5554a016104
analistakatia <katiadocita@gmail.com>
2022-06-30 18:04:49 -0300

    Entrega Média Concluida

--- src/desafio01/SistemaSorteio.java
+++ src/desafio01/SistemaSorteio.java
@@ -3,71 +3,62 @@ import java.util.Scanner;
 
 public class SistemaSorteio {
     public static void main(String[] args) {
-
+        int possibilidades = 100;
         Random aleatorio = new Random();
-        int valor = aleatorio.nextInt(10) + 1;
+        int valor = aleatorio.nextInt(possibilidades) + 1;
         int valormenor = valor - 1;
         int valormaior = valor + 1;
-        int acerto = 10;
-        int aproximado = 5;
+        int pontuacao = 0;
 
         Scanner numInformado = new Scanner(System.in);
-        System.out.print("Escolha um número de 1 a 10: ");
+        System.out.print("Escolha um número de 1 a " + possibilidades + ": ");
         int num = numInformado.nextInt();
         System.out.println("O número sorteado é: " + valor);
 
-        if (num == valor){
-            System.out.println("Você acertou o número exato! Parabéns!!!\nVocê ganhou 10 pontos!");
-        } else if (num == valormaior) {
-            System.out.println("Você errou por um número!! \nPorém, como errou por um número acima, você ganhou 5 pontos por quase acertar!");
-        } else if (num == valormenor) {
-            System.out.println("Você errou por um número!! \nPorém, como errou por um número abaixo, você perdeu 5 pontos :(");
+        //int resultado = numInformado.nextInt();
+        if (num == valor) {
+            System.out.println("Parabéns!! Você  acertou o número exato!!!\nVocê ganhou 10 pontos!");
+            pontuacao = pontuacao + 10;
+        } else if (num == valormaior || num == valormenor) {
+            System.out.println("Você errou por um número!! \nVocê ganhou 5 pontos por quase acertar!");
+            pontuacao = pontuacao + 5;
         } else {
             System.out.println("Poxa...Você não teve muita sorte :( \nMas não desista, na próxima você consegue!!");
         }
-        /*Scanner continuar = new Scanner(System.in);
-        System.out.println("Deseja continuar tentando?");
-        System.out.println("1. Continuar");
-        System.out.println("2. Sair");
+        System.out.println("Sua pontuação é: " + pontuacao);
+
+        Scanner continuar = new Scanner(System.in);
+        System.out.println("Deseja continuar tentando? Digite s para sim ou n não");
         String escolha = continuar.next();
 
-        int somaPto1 = acerto + acerto;
-        int somaPto2 = aproximado + acerto;
-        int SomaPto3 = aproximado + aproximado;
 
-        while (escolha == "1"){
-            //Scanner numInformado = new Scanner(System.in);
-            System.out.print("Escolha um número de 1 a 10: ");
-            //int num = numInformado.nextInt();
+
+        while (escolha.equals("s")) {
+            System.out.print("Escolha um número de 1 a " +possibilidades+ ": ");
+            num = numInformado.nextInt();
+            aleatorio = new Random();
+            valor = aleatorio.nextInt(possibilidades) + 1;
+            valormenor = valor - 1;
+            valormaior = valor + 1;
             System.out.println("O número sorteado é: " + valor);
 
-            if (num == valor){
-                int tot1 = somaPto1;
-                System.out.println("Você acertou o número exato! Parabéns!!!\nVocê ganhou 10 pontos!");
-                System.out.println("Você está com " + tot1 + " pontos.");
-            } else if (num == valormenor || num == valormaior) {
-                int tot2 =
+            if (num == valor) {
+                System.out.println("Parabéns!! Você  acertou o número exato!!!\nVocê ganhou 10 pontos!");
+                pontuacao = pontuacao + 10;
+            } else if (num == valormaior || num == valormenor) {
                 System.out.println("Você errou por um número!! \nVocê ganhou 5 pontos por quase acertar!");
-            }else {
+                pontuacao = pontuacao + 5;
+            } else {
                 System.out.println("Poxa...Você não teve muita sorte :( \nMas não desista, na próxima você consegue!!");
             }
+            System.out.println("Sua pontuação é: " + pontuacao);
+
+            System.out.println("Deseja continuar tentando? Digite s para sim ou n não");
+            escolha = continuar.next();
+
+            //System.out.println("Fim de jogo!!");
+
+
         }
-
-        switch (escolha){
-            case "1":
-
-                while (escolha = )
-        }
-
-
-        String sim = continuar.next();
-        String nao = continuar.next();
-        String respSim = "S";
-        String respNao = "N";
-
-        if (sim == respSim){
-            int somaPonto =
-        }*/
-
     }
 }
